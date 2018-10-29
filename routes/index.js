8e769980fdbe85c057dc6ba381b54cf0a10a0160
Sxians <l>
2018-10-29 17:41:48 +0800

    koa2 发布文章接口

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const Router = require("koa-router");
+const ArticleController = require("../controllers/article");
+
+const router = new Router({
+  prefix: "/api/v1"
+});
+
+/**
+ * 文章接口
+ */
+// 创建文章接口（路由）
+router.post("/article", ArticleController.create);
+// 获取文章详情接口（路由）
+router.get("/article/:id", ArticleController.detail);
+
+module.exports = router;
